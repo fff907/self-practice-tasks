6c26b12b464dda3e8d2ce61d466f6768791b172a
fff907 <59164817+fff907@users.noreply.github.com>
2025-06-09 16:48:12 +0900

    commit

--- Day4/main.js
+++ Day4/main.js
@@ -1,6 +1,6 @@
 const items = document.querySelectorAll("#colorList li");
 const colors = ["red","blue","green","yellow"];
 
-for (let i = 0; i < items.length; i++) {
-    items[i].style.backgroundColor = colors[i];
-}
+colors.forEach((fruit) => {
+  console.log(fruit);
+});
