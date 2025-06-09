a745383e9934f0462ba020700c788ad26197fc21
fff907 <59164817+fff907@users.noreply.github.com>
2025-06-09 17:04:52 +0900

    commit

--- Day4/main.js
+++ Day4/main.js
@@ -2,9 +2,7 @@ const items = document.querySelectorAll("#colorList li");
 const colors = ["red","blue","green","yellow"];
 
 items.forEach((item, index) => {
-    item.style.backgroundColor = colors[index];
-
     item.addEventListener("click", () => {
-        alert(`${item.textContent}をクリックしました！`);
+           item.style.backgroundColor = colors[index];
     });
 });
