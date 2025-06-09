946f034de672567888bc010b60ad1516eda8887f
fff907 <59164817+fff907@users.noreply.github.com>
2025-06-09 17:13:34 +0900

    commit

--- main.js
+++ main.js
@@ -3,6 +3,14 @@ const colors = ["red","blue","green","yellow"];
 
 items.forEach((item, index) => {
     item.addEventListener("click", () => {
-           item.style.backgroundColor = colors[index];
+        items.forEach(i => {
+            i.classList.remove("active");
+            i.style.backgroundColor = "";
+            i.style.color = "";
+        });
+
+        item.style.backgroundColor = colors[index];
+        item.style.color = "white";
+        item.classList.add("active");
     });
 });
