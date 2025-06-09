c6b9b3d7cd0c3f356b733e19b191780515a29c45
fff907 <59164817+fff907@users.noreply.github.com>
2025-06-09 16:37:51 +0900

    commit

--- Day4/main.js
+++ Day4/main.js
@@ -2,5 +2,5 @@ const items = document.querySelectorAll("#colorList li");
 const colors = ["red","blue","green","yellow"];
 
 for (let i = 0; i < items.length; i++) {
-    items[i].computedStyleMap.backgroundColor = colors[i];
+    items[i].style.backgroundColor = colors[i];
 }
