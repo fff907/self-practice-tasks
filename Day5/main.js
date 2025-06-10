3e08a01657a3e779bdd8372eb50a87822b0398e8
fff907 <59164817+fff907@users.noreply.github.com>
2025-06-10 14:17:44 +0900

    commit

--- Day5/main.js
+++ Day5/main.js
@@ -1,6 +1,16 @@
+let clickCount = 0;
+
 function changeText() {
     const output = document.getElementById('outputText');
-    output.textContent = '変更されました！';
+    clickCount++;
+
+    if (clickCount === 1) {
+        output.textContent = '最初のクリックですね！';
+    } else if (clickCount === 2) {
+        output.textContent = '2回目のクリックです！';
+    } else {
+        output.textContent = 'もう${clickCount}回目ですよ～！';
+    }
 }
 
 document.getElementById('changeBtn').addEventListener('click', changeText);
